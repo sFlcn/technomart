64691b030195899feffc67d5ebbe02ed9b5b76e9
sFlcn <57070095+sFlcn@users.noreply.github.com>
2020-03-22 22:42:07 +0300

    Настройка поведения формы обратной связи и localStorage

--- js/script.js
+++ js/script.js
@@ -4,13 +4,44 @@ var popupWriteUs = document.querySelector(".popup--write-us");
 if (popupWriteUs) {
   var buttonWriteUs = document.querySelector(".contacts__link");
   var popupWriteUsClose = popupWriteUs.querySelector(".popup-close");
+  var formWriteUs = popupWriteUs.querySelector(".popup__form");
+  var messageNameWriteUs = popupWriteUs.querySelector("[name=message-name]");
+  var messageEmailWriteUs = popupWriteUs.querySelector("[name=message-email]");
+  var messageWriteUs = popupWriteUs.querySelector("[name=message]");
+
+  var isStorageSupport = true;
+  var storage = "";
+    try {
+      storage = localStorage.getItem("messageNameWriteUs");
+    } catch (err) {
+      isStorageSupport = false;
+    }
+
   buttonWriteUs.addEventListener("click", function (evt) {
     evt.preventDefault();
     popupWriteUs.removeAttribute("hidden");
+    if (storage) {
+      messageNameWriteUs.value = storage;
+      messageEmailWriteUs.focus();
+    } else {
+      messageNameWriteUs.focus();
+    }
   });
+
   popupWriteUsClose.addEventListener("click", function (evt) {
     evt.preventDefault();
     popupWriteUs.setAttribute("hidden","");
+  });
+
+  formWriteUs.addEventListener("submit", function (evt) {
+    if (!messageWriteUs.value) {
+      evt.preventDefault();
+      console.log("Нужно ввести логин и пароль");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("messageNameWriteUs", messageNameWriteUs.value);
+      }
+    }
   });
 }
 
@@ -20,6 +51,7 @@ var popupMap = document.querySelector(".popup--map");
 if (popupMap) {
   var buttonMap = document.querySelector(".contacts__map");
   var popupMapClose = popupMap.querySelector(".popup-close");
+
   buttonMap.addEventListener("click", function (evt) {
     evt.preventDefault();
     popupMap.removeAttribute("hidden");
@@ -38,12 +70,14 @@ if (popupAddToCart) {
   var buttonsAddToCart = document.querySelectorAll(".product__button--buy");
   var popupAddToCartClose = popupAddToCart.querySelector(".popup-close");
   var popupAddToCartContinue = popupAddToCart.querySelector(".cart-continue");
+
   for (var i = 0; i < buttonsAddToCart.length; i++) {
     buttonsAddToCart[i].addEventListener("click", function (evt) {
       evt.preventDefault();
       popupAddToCart.removeAttribute("hidden");
     });
   }
+
   popupAddToCartClose.addEventListener("click", function (evt) {
     evt.preventDefault();
     popupAddToCart.setAttribute("hidden","");
@@ -64,6 +98,7 @@ if (document.querySelector(".login-panel")) {
   var buttonUserExit = document.querySelector(".login-panel__user-logout");
   var buttonUserOrders = document.querySelector(".login-panel__user-orders");
   var buttonUserProfile = document.querySelector(".login-panel__user-profile");
+
   buttonUserEnter.addEventListener("click", function (evt) {
     evt.preventDefault();
     buttonUserEnter.classList.add("login-panel__item--hidden");
