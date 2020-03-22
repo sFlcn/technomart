cc15cf713dc4bf371ca13f4d61513d151cdda039
sFlcn <57070095+sFlcn@users.noreply.github.com>
2020-03-22 22:03:05 +0300

    Улучшен скрипт карты и связи, добавлен для покупки и для входа

--- js/script.js
+++ js/script.js
@@ -1,28 +1,86 @@
+// Обратная связь
+
 var popupWriteUs = document.querySelector(".popup--write-us");
-var writeUsCall = document.querySelector(".contacts__link");
-var writeUsClose = document.querySelector(".popup--write-us .popup-close");
+if (popupWriteUs) {
+  var buttonWriteUs = document.querySelector(".contacts__link");
+  var popupWriteUsClose = popupWriteUs.querySelector(".popup-close");
+  buttonWriteUs.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popupWriteUs.removeAttribute("hidden");
+  });
+  popupWriteUsClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popupWriteUs.setAttribute("hidden","");
+  });
+}
+
+// Карта
+
 var popupMap = document.querySelector(".popup--map");
-var mapCall = document.querySelector(".contacts__map");
-var mapClose = document.querySelector(".popup--map .popup-close");
+if (popupMap) {
+  var buttonMap = document.querySelector(".contacts__map");
+  var popupMapClose = popupMap.querySelector(".popup-close");
+  buttonMap.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popupMap.removeAttribute("hidden");
+  });
 
-writeUsCall.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupWriteUs.removeAttribute("hidden");
-});
+  popupMapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popupMap.setAttribute("hidden","");
+  });
+}
 
-writeUsClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupWriteUs.setAttribute("hidden","");
-});
+// Корзина
 
+var popupAddToCart = document.querySelector(".popup--cart");
+if (popupAddToCart) {
+  var buttonsAddToCart = document.querySelectorAll(".product__button--buy");
+  var popupAddToCartClose = popupAddToCart.querySelector(".popup-close");
+  var popupAddToCartContinue = popupAddToCart.querySelector(".cart-continue");
+  for (var i = 0; i < buttonsAddToCart.length; i++) {
+    buttonsAddToCart[i].addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popupAddToCart.removeAttribute("hidden");
+    });
+  }
+  popupAddToCartClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popupAddToCart.setAttribute("hidden","");
+  });
 
+  popupAddToCartContinue.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popupAddToCart.setAttribute("hidden","");
+  });
+}
 
-mapCall.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupMap.removeAttribute("hidden");
-});
+// Логин
 
-mapClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupMap.setAttribute("hidden","");
-});
+if (document.querySelector(".login-panel")) {
+  var buttonUserEnter = document.querySelector(".login-panel__enter");
+  var buttonUserReg = document.querySelector(".login-panel__registration");
+  var buttonUserName = document.querySelector(".login-panel__user-name");
+  var buttonUserExit = document.querySelector(".login-panel__user-logout");
+  var buttonUserOrders = document.querySelector(".login-panel__user-orders");
+  var buttonUserProfile = document.querySelector(".login-panel__user-profile");
+  buttonUserEnter.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    buttonUserEnter.classList.add("login-panel__item--hidden");
+    buttonUserReg.classList.add("login-panel__item--hidden");
+    buttonUserName.classList.remove("login-panel__item--hidden");
+    buttonUserExit.classList.remove("login-panel__item--hidden");
+    buttonUserOrders.classList.remove("login-panel__item--hidden");
+    buttonUserProfile.classList.remove("login-panel__item--hidden");
+  });
+  buttonUserExit.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    buttonUserEnter.classList.remove("login-panel__item--hidden");
+    buttonUserReg.classList.remove("login-panel__item--hidden");
+    buttonUserName.classList.add("login-panel__item--hidden");
+    buttonUserExit.classList.add("login-panel__item--hidden");
+    buttonUserOrders.classList.add("login-panel__item--hidden");
+    buttonUserProfile.classList.add("login-panel__item--hidden");
+  });
+
+}
